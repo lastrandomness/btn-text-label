26a88155d84e82f54d91f6b64d7669d02cfb543a
MZA <last.randomness@gmail.com>
2019-01-09 02:16:40 -0800

    Initial Commit

---
+++ myIdeaApp1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myIdeaApp1
+//
+//  Created by TikTak on 1/9/19.
+//  Copyright © 2019 TikTak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
