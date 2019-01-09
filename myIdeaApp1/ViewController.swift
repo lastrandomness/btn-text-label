4f32085c5449aa99e6934f2e253cc39ee3f7a047
MZA <last.randomness@gmail.com>
2019-01-09 04:07:05 -0800

    button text label

--- myIdeaApp1/ViewController.swift
+++ myIdeaApp1/ViewController.swift
@@ -9,12 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var fsize = 10
+    enum action {
+        case small
+        case large
+    }
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var myText: UITextField!
+    @IBAction func largerBtn(_ sender: Any) {
+        txtToLabel()
+        fsize = fsize + 3
+        myLabel.font = UIFont(name: myLabel.font.fontName, size: CGFloat(update(xFont:.large)))
+        print("fsize = \(fsize)")
+    }
+    @IBAction func blueBtn(_ sender: Any) {
+        txtToLabel()
+        myLabel.textColor = UIColor.blue
+        
+    }
+    
+    @IBAction func smallerBtn(_ sender: Any) {
+        txtToLabel()
+        
+        //var fontSizeSaved = largerF(xFont:10)
+        myLabel.font = UIFont(name: myLabel.font.fontName, size:CGFloat(update(xFont:.small)))
+        //CGFloat(largerF(xFont:10))
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    func txtToLabel() {
+        myLabel.text = myText.text
+    }
+    func update(xFont:action)->Int {
+        if (xFont == .small && fsize >= 1){
+        fsize -= 3
+            
+    }
+        if(xFont == .large){
+            fsize += 3
+        }
+return fsize
+    
 
 }
-
+}
